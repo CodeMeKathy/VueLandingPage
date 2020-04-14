0cb07ba124f0050eb71fb9c364f2dd5536dac7f6
Kathy * <Kathy@KathyLambert.me>
2020-04-14 11:32:46 -0400

    Removing node modules and package-lock.json file and replacing it with older package.json file from commit: 7f8841

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer-div">
-    <div class="left-col">© 2020 Huddle</div>
+    <div class="left-col heading-secondary">© 2020 Huddle</div>
     <div class="right-col">
       Made by CodeMeKathy && Designed by Frontend Mentors
     </div>
@@ -13,7 +13,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+@import '@/sass/main.scss';
+
 .footer-div {
   display: flex;
   justify-content: space-between;
